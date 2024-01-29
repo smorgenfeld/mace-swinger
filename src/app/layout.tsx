28d357b0dfb202081c9f11451ccad8634abb6d10
smorgenfeld <smorgenfeld@users.noreply.github.com>
2024-01-28 22:25:52 -0800

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Mace Swinger",
+  description: "Description",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-512x512.png" />
+        <meta name="theme-color" content="#000" />
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
