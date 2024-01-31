3b32c1d5d4abb98aa837d4b060c929ec3ec52c20
smorgenfeld <smorgenfeld@users.noreply.github.com>
2024-01-30 22:30:03 -0800

    mildly broken navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,42 @@
+'use client'
 import Image from "next/image";
+import React from 'react';
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
 
-export default function Home() {
+export default function App() {
+  const [horzSlider] = useKeenSlider<HTMLDivElement>({
+    slides: {
+      origin: "center",
+    },
+    selector: ".first > .keen-slider__slide",
+  })
+
+
+  const [vertSlider] = useKeenSlider<HTMLDivElement>({
+    rubberband: false,
+    slides: {
+      perView: 1,
+      origin: "center",
+    },
+    vertical: true,
+  })
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      yeet3
-    </main>
-  );
+    <div ref={horzSlider} className="keen-slider first">
+      <div className="keen-slider__slide number-slide1">BASE UPGRADE</div>
+      <div className="keen-slider__slide number-slide2">
+        <div
+          ref={vertSlider}
+          className="keen-slider"
+          style={{ width: "100%", height: "100vh" }}
+        >
+          <div className="keen-slider__slide number-slide1">MAP</div>
+          <div className="keen-slider__slide number-slide2">BASE</div>
+          <div className="keen-slider__slide number-slide3">DUNGEON</div>
+        </div>
+      </div>
+      <div className="keen-slider__slide number-slide4">INVENTORY</div>
+    </div>
+  )
 }
